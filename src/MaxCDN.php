3e2175af78150d6d15be064d4faf0f40fbb218d2
Stefano Plompen <stefano@blueberry.nl>
2018-10-30 16:29:25 +0100

    - setTimeout to 10 seconds instead of 60 seconds

--- src/MaxCDN.php
+++ src/MaxCDN.php
@@ -1,35 +1,35 @@
 <?php
-/** 
+/**
  * MaxCDN REST Client Library
- * 
+ *
  * @copyright 2012
  * @author Karlo Espiritu
  * @version 1.0 2012-09-21
 */
 class MaxCDN {
-	
+
 	public $alias;
 
 	public $key;
 
 	public $secret;
-	
+
 	public $MaxCDNrws_url = 'https://rws.maxcdn.com';
-	
+
     private $consumer;
-	
+
 	public function __construct($alias, $key, $secret, $options=null) {
 		$this->alias  = $alias;
 		$this->key    = $key;
 		$this->secret = $secret;
 		$this->consumer = new \MaxCDN\OAuth\OAuthConsumer($key, $secret, NULL);
-		
+
 	}
 
 	private function execute($selected_call, $method_type, $params) {
 		// the endpoint for your request
-		$endpoint = "$this->MaxCDNrws_url/$this->alias$selected_call"; 
-		
+		$endpoint = "$this->MaxCDNrws_url/$this->alias$selected_call";
+
 		//parse endpoint before creating OAuth request
 		$parsed = parse_url($endpoint);
 		if (array_key_exists("parsed", $parsed))
@@ -44,18 +44,18 @@ class MaxCDN {
 		$sig_method = new \MaxCDN\OAuth\OAuthSignatureMethod_HMAC_SHA1();
 		$req_req->sign_request($sig_method, $this->consumer, NULL);
 
-		// create curl resource 
-		$ch = curl_init(); 
+		// create curl resource
+		$ch = curl_init();
 		$url_parts = explode('?',$req_req);
 		// set url
 		curl_setopt($ch, CURLOPT_URL, $url_parts[0]);
-		
+
 		//return the transfer as a string
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER , TRUE);
-		
+
 		// set curl timeout
-		curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
 		// set curl custom request type if not standard
 		if ($method_type != "GET" && $method_type != "POST") {
@@ -72,7 +72,7 @@ class MaxCDN {
 		// retrieve headers
 		curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
-		
+
 		//set user agent
 		curl_setopt($ch, CURLOPT_USERAGENT, 'PHP MaxCDN API Client');
 
@@ -81,36 +81,36 @@ class MaxCDN {
 		$headers = curl_getinfo($ch);
 		$curl_error = curl_error($ch);
 
-		// close curl resource to free up system resources 
+		// close curl resource to free up system resources
 		curl_close($ch);
 
-		// $json_output contains the output string 
+		// $json_output contains the output string
 		$json_output = substr($result, $headers['header_size']);
 
 		// catch errors
-		if(!empty($curl_error) || empty($json_output)) { 
+		if(!empty($curl_error) || empty($json_output)) {
 			throw new \MaxCDN\RWSException("CURL ERROR: $curl_error, Output: $json_output", $headers['http_code'], null, $headers);
 		}
 
 		return $json_output;
 	}
-	
+
 	public function get($selected_call, $params = array()){
-		 
+
 		return $this->execute($selected_call, 'GET', $params);
 	}
-	
+
 	public function post($selected_call, $params = array()){
 		return $this->execute($selected_call, 'POST', $params);
 	}
-	
+
 	public function put($selected_call, $params = array()){
 		return $this->execute($selected_call, 'PUT', $params);
 	}
-	
+
 	public function delete($selected_call, $params = array()){
 		return $this->execute($selected_call, 'DELETE', $params);
 	}
-	
-	
+
+
 }
